e8a74a2e6a63fd69665c8937971b653334a58f4b
maahmed <mahmood1995ahmed@outlook.com>
2023-08-16 13:10:53 +0300

    sound

---
+++ js-soundboard-start/scripts/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $("#after").on("click", function () {
+    $("#afterA")[0].play();
+  });
+  $("#better").on("click", function () {
+    $("#betterA")[0].play();
+  });
+  $("#doIt").on("click", function () {
+    $("#doItA")[0].play();
+  });
+  $("#ever").on("click", function () {
+    $("#everA")[0].play();
+  });
+  $("#faster").on("click", function () {
+    $("#fasterA")[0].play();
+  });
+  $("#harder").on("click", function () {
+    $("#harderA")[0].play();
+  });
+  $("#hour").on("click", function () {
+    $("#hourA")[0].play();
+  });
+  $("#makeIt").on("click", function () {
+    $("#makeItA")[0].play();
+  });
+  $("#makesUs").on("click", function () {
+    $("#makesUsA")[0].play();
+  });
+  $("#moreThan").on("click", function () {
+    $("#moreThanA")[0].play();
+  });
+  $("#never").on("click", function () {
+    $("#neverA")[0].play();
+  });
+  $("#our").on("click", function () {
+    $("#ourA")[0].play();
+  });
+  $("#over").on("click", function () {
+    $("#overA")[0].play();
+  });
+  $("#stronger").on("click", function () {
+    $("#strongerA")[0].play();
+  });
+  $("#workIs").on("click", function () {
+    $("#workIsA")[0].play();
+  });
+  $("#workIt").on("click", function () {
+    $("#workItA")[0].play();
+  });
+});
